f5e60525436c43f429683ed3b942842bfa46d960
Johan Sørensen <johan@johansorensen.com>
2016-02-23 21:24:13 +0100

    Initial Commit

---
+++ WaddaColor/WaddaColor.h
@@ -0,0 +1,19 @@
+//
+//  WaddaColor.h
+//  WaddaColor
+//
+//  Created by Johan Sørensen on 23/02/16.
+//  Copyright © 2016 Johan Sørensen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for WaddaColor.
+FOUNDATION_EXPORT double WaddaColorVersionNumber;
+
+//! Project version string for WaddaColor.
+FOUNDATION_EXPORT const unsigned char WaddaColorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WaddaColor/PublicHeader.h>
+
+
