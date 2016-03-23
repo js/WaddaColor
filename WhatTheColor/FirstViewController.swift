0939bed1e122fb00d0147435ea4817d5af1ffa26
Johan Sørensen <johan@johansorensen.com>
2016-03-23 23:53:06 +0100

    Render complementary color in the example project

--- WhatTheColor/FirstViewController.swift
+++ WhatTheColor/FirstViewController.swift
@@ -11,7 +11,7 @@ import WaddaColor
 
 class FirstViewController: UIViewController {
     @IBOutlet weak var tableView: UITableView!
-    let colors: [(name: String, rgba: RGBA)] = ColorThesaurusNames().map({ (name: $0, rgba: $1) })
+    let colors: [(name: String, rgba: RGBA)] = ColorThesaurusNames().map({ (name: $0, rgba: $1) }).sort({ $0.name < $1.name })
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,24 +19,47 @@ class FirstViewController: UIViewController {
     }
 }
 
-extension FirstViewController: UITableViewDataSource {
+extension FirstViewController: UITableViewDataSource, UITableViewDelegate {
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return colors.count
     }
 
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier("ColorCell", forIndexPath: indexPath)
-        let color = colors[indexPath.row]
-        cell.textLabel?.text = color.name
-        let hsl = HSL(rgb: color.rgba)
-        if hsl.isLight() {
-            cell.textLabel?.textColor = UIColor.blackColor()
-        } else {
-            cell.textLabel?.textColor = UIColor.whiteColor()
-        }
-        cell.textLabel?.backgroundColor = color.rgba.color
-        cell.contentView.backgroundColor = color.rgba.color
+        let cell = tableView.dequeueReusableCellWithIdentifier(ColorTableViewCell.identifier, forIndexPath: indexPath) as! ColorTableViewCell
+        let colorTuple = colors[indexPath.row]
+        let color = WaddaColor(values: colorTuple.rgba)
+        cell.colorView.backgroundColor = color.color
+        cell.colorLabel.text = colorTuple.name
+
+        cell.contrastingColorView.backgroundColor = color.contrastingColor().color
+        cell.complementaryColorView.backgroundColor = color.complementaryColor().color
+
+
         return cell
+    }
+
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        tableView.deselectRowAtIndexPath(indexPath, animated: false)
+    }
+
+    func tableView(tableView: UITableView, shouldHighlightRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        return false
     }
 }
 
+class ColorTableViewCell: UITableViewCell {
+    static let identifier = "ColorTableViewCell"
+
+    @IBOutlet weak var colorView: UIView!
+    @IBOutlet weak var colorLabel: UILabel!
+    @IBOutlet weak var complementaryColorView: UIView!
+    @IBOutlet weak var contrastingColorView: UIView!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+
+        [colorView, complementaryColorView, contrastingColorView].forEach { view in
+            view.layer.cornerRadius = view.bounds.width / 2
+        }
+    }
+}
